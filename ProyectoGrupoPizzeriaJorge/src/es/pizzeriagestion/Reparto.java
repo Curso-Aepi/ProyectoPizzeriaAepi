77d1c0bf4f08945be8c6e5c5dad2771e2d237055
lpaton <109732102+lpaton@users.noreply.github.com>
2022-07-22 17:03:03 +0200

    Update Reparto.java

--- ProyectoGrupoPizzeriaJorge/src/es/pizzeriagestion/Reparto.java
+++ ProyectoGrupoPizzeriaJorge/src/es/pizzeriagestion/Reparto.java
@@ -17,18 +17,19 @@ public class Reparto extends Pizzeria implements ICalcular {
 	/**
 	 * Variables de la clase Reparto
 	 * 
-	 * @param numeroDeRepartidores. Número de repartidores de la pizzería
+	 * 
 	 * @param tipoVehiculo.         Tipo de vehículo usado en el reparto
 	 * @param distancia.            Distancia del domicilio en kilómetros
 	 * @param precioGasolina.       Precio de la gasolina
-	 * @param estadoPedido.         Estado del pedido (entrega o pendiente)
+	 * @param numeroDeRepartidores. Número de repartidores de la pizzería
 	 *
 	 */
-	int numeroDeRepartidores;
-	String tipoVehiculo;
+
+	int tipoVehiculo;
 	double distancia;
 	double precioGasolina;
-	boolean estadoPedido;
+	int numeroDeRepartidores;
+
 	public static final double CONSUMO_MOTO = 0.025;
 	public static final double CONSUMO_COCHE = 0.05;
 	public static final int TIEMPO_PREPARACION = 15;
@@ -47,11 +48,7 @@ public class Reparto extends Pizzeria implements ICalcular {
 	 * 
 	 * Método constructor con argumentos
 	 * 
-	 * @param nombre               Nombre de la pizzería
-	 * @param numeroDeTelefono     Número de teléfono de la pizzería
-	 * @param horario              Horario de la pizzería
-	 * @param direccion            Dirección de la pizzería
-	 * @param numeroDeEmpleados    Número de empleados totales
+	 * 
 	 * @param numeroDeRepartidores Número de repartidores a domicilio
 	 * @param tipoVehiculo         Tipo de vehículo usado para repartir (moto o
 	 *                             coche)
@@ -61,38 +58,13 @@ public class Reparto extends Pizzeria implements ICalcular {
 	 * @param estadoPedido         Estado del pedido (true para entregado, false
 	 *                             para pendiente)
 	 */
-	public Reparto(String nombre, String numeroDeTelefono, String horario, String direccion, int numeroDeEmpleados,
-			int numeroDeRepartidores, String tipoVehiculo, double distancia, double precioGasolina,
-			boolean estadoPedido) {
-		super(nombre, numeroDeTelefono, horario, direccion, numeroDeEmpleados);
-		this.numeroDeRepartidores = numeroDeRepartidores;
+	public Reparto(int tipoVehiculo, double distancia, double precioGasolina, int numeroDeRepartidores) {
+		super();
+
 		this.tipoVehiculo = tipoVehiculo;
 		this.distancia = distancia;
 		this.precioGasolina = precioGasolina;
-		this.estadoPedido = estadoPedido;
-	}
-
-	/**
-	 * Devuelve el número de repartidores
-	 * 
-	 * @return Devuelve el número de repartidores
-	 */
-	public int getNumeroDeRepartidores() {
-		return numeroDeRepartidores;
-	}
-
-	/**
-	 * Establece el número de repartidores disponibles. Si no hay ninguno disponible
-	 * no se podrán hacer repartos a domicilio
-	 * 
-	 * @param numeroDeRepartidores
-	 */
-	public void setNumeroDeRepartidores(int numeroDeRepartidores) {
-		if (numeroDeRepartidores > 0) {
-			this.numeroDeRepartidores = numeroDeRepartidores;
-		} else {
-			System.out.println("No hay repartidores disponibles en este momento");
-		}
+		this.numeroDeRepartidores = numeroDeRepartidores;
 
 	}
 
@@ -101,7 +73,7 @@ public class Reparto extends Pizzeria implements ICalcular {
 	 * 
 	 * @return Devuelve el tipo de vehículo usado en el reparto
 	 */
-	public String getTipoVehiculo() {
+	public int getTipoVehiculo() {
 		return tipoVehiculo;
 	}
 
@@ -110,11 +82,11 @@ public class Reparto extends Pizzeria implements ICalcular {
 	 * 
 	 * @param tipoVehiculo
 	 */
-	public void setTipoVehiculo(String tipoVehiculo) {
-		if (tipoVehiculo.equals("moto") && tipoVehiculo.equals("coche")) {
+	public void setTipoVehiculo(int tipoVehiculo) {
+		if (tipoVehiculo == 1 || tipoVehiculo == 2) {
 			this.tipoVehiculo = tipoVehiculo;
 		} else {
-			System.out.println("Solo tenemos reparto en moto y coche. Revísalo");
+			System.out.println("Solo tenemos reparto en moto[1] y coche[2]. Revísalo");
 		}
 
 	}
@@ -163,21 +135,27 @@ public class Reparto extends Pizzeria implements ICalcular {
 	}
 
 	/**
-	 * Devuelve el estado del pedido
+	 * Devuelve el número de repartidores
 	 * 
-	 * @return Devuelve el estado del pedido
+	 * @return Devuelve el número de repartidores
 	 */
-	public boolean getEstadoPedido() {
-		return estadoPedido;
+	public int getNumeroDeRepartidores() {
+		return numeroDeRepartidores;
 	}
 
 	/**
-	 * Establece el estado del pedido (true para entregado)
+	 * Establece el número de repartidores disponibles. Si no hay ninguno disponible
+	 * no se podrán hacer repartos a domicilio
 	 * 
-	 * @param estadoPedido
+	 * @param numeroDeRepartidores
 	 */
-	public void setEstadoPedido(boolean estadoPedido) {
-		this.estadoPedido = estadoPedido;
+	public void setNumeroDeRepartidores(int numeroDeRepartidores) {
+		if (numeroDeRepartidores > 0) {
+			this.numeroDeRepartidores = numeroDeRepartidores;
+		} else {
+			System.out.println("No hay repartidores disponibles en este momento");
+		}
+
 	}
 
 	/**
@@ -228,10 +206,11 @@ public class Reparto extends Pizzeria implements ICalcular {
 	 * Método toString
 	 * 
 	 */
+
 	@Override
 	public String toString() {
-		return "Reparto [numeroDeRepartidores=" + numeroDeRepartidores + ", tipoVehiculo=" + tipoVehiculo
-				+ ", distancia=" + distancia + ", precioGasolina=" + precioGasolina + "]";
+		return "Reparto [tipoVehiculo=" + tipoVehiculo + ", distancia=" + distancia + ", precioGasolina="
+				+ precioGasolina + ", numeroDeRepartidores=" + numeroDeRepartidores + "]";
 	}
 
 	/***
@@ -245,38 +224,24 @@ public class Reparto extends Pizzeria implements ICalcular {
 
 	public double tiempoDeEntrega() {
 		double tiempoEntrega = 0;
+		if (this.numeroDeRepartidores <= 0) {
+			System.out.println("No hay repartidores disponibles en este momento. Entrega pendiente");
+		} else {
+
+		}
 		if (this.distancia > 0 && this.distancia <= 5) {
-			tiempoEntrega = ((UNA_HORA * 5) / VELOCIDAD_POBLADO) + TIEMPO_PREPARACION;
-		}
-		if (this.distancia > 5 && this.distancia <= 10) {
-			tiempoEntrega = ((UNA_HORA * 10) / VELOCIDAD_POBLADO) + TIEMPO_PREPARACION;
-		}
-		if (this.distancia > 10 && this.distancia <= 15) {
-			tiempoEntrega = ((UNA_HORA * 15) / VELOCIDAD_POBLADO) + TIEMPO_PREPARACION;
-		}
-		if (this.distancia > 15 && this.distancia <= 20) {
-			tiempoEntrega = ((UNA_HORA * 20) / VELOCIDAD_POBLADO) + TIEMPO_PREPARACION;
+			tiempoEntrega = (UNA_HORA * 5) / VELOCIDAD_POBLADO + TIEMPO_PREPARACION;
+		} else if (this.distancia > 5 && this.distancia <= 10) {
+			tiempoEntrega = (UNA_HORA * 10) / VELOCIDAD_POBLADO + TIEMPO_PREPARACION;
+		} else if (this.distancia > 10 && this.distancia <= 15) {
+			tiempoEntrega = (UNA_HORA * 15) / VELOCIDAD_POBLADO + TIEMPO_PREPARACION;
+		} else if (this.distancia > 15 && this.distancia <= 20) {
+			tiempoEntrega = (UNA_HORA * 20) / VELOCIDAD_POBLADO + TIEMPO_PREPARACION;
+		} else {
+			System.out.println("No es posible realizar la entrega a más de 20 kilómetros de distancia");
 		}
 
 		return tiempoEntrega;
-
-	}
-
-	/**
-	 * Método entregaPedido para indicar si el pedido está entregado o pendiente. Si
-	 * el pedido está entregado el estado es true y false para pendiente de entrega.
-	 * 
-	 * @return Devuelve si el pedido está o no entregado
-	 */
-
-	public String entregaPedido() {
-		String mensaje = "";
-		if (this.estadoPedido == true) {
-			mensaje = "Pedido entregado";
-		} else {
-			mensaje = "Pedido pendiente de entrega";
-		}
-		return mensaje;
 
 	}
 
@@ -292,14 +257,17 @@ public class Reparto extends Pizzeria implements ICalcular {
 	@Override
 	public double calcular() {
 		double gastoGasolina = 0;
-		if (this.tipoVehiculo.equals("moto")) {
+
+		switch (this.tipoVehiculo) {
+		case 1: {
 			gastoGasolina = (CONSUMO_MOTO * this.distancia * precioGasolina) * 2;
-
+			break;
 		}
-		if (this.tipoVehiculo.equals("coche")) {
+		case 2:
 			gastoGasolina = (CONSUMO_COCHE * this.distancia * precioGasolina) * 2;
-
+			break;
 		}
+
 		return gastoGasolina;
 
 	}
